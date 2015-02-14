74c4b13f017d65fa190be67f473395da47560402
Richard Wall <richard@the-moon.net>
2015-02-14 22:19:20 +0000

    Some integration test skeleton functions

--- test/takehome/integration.clj
+++ test/takehome/integration.clj
@@ -13,4 +13,38 @@
 
  (fact "Healthcheck resource returns 200 HTTP response"
        (let [response (http/get (url+ "/healthcheck") {:throw-exceptions false})]
-         response => (contains {:status 200}))))
+         response => (contains {:status 200})))
+
+ (fact "Hello resource returns 200 HTTP response"
+       (let [response (http/get (url+ "/hellos?nickname=world") {:throw-exceptions false})]
+         response => (contains {:status 200})))
+
+ (fact "Subscribe to topic returns 200 HTTP response"
+       (let [response (http/post (url+ "/kittens_and_puppies/alice") {:throw-exceptions false})]
+         response => (contains {:status 200})))
+
+ (fact "Unsubscribe from topic returns 200 HTTP response"
+       (let [response (http/delete (url+ "/kittens_and_puppies/alice") {:throw-exceptions false})]
+         response => (contains {:status 200})))
+
+ (fact "Unsubscribe from unknown topic returns 404 HTTP response"
+       (let [response (http/delete (url+ "/unknown_topic/alice") {:throw-exceptions false})]
+         response => (contains {:status 404})))
+
+ (fact "Publish a message to topic returns 200 HTTP response"
+       (let [response (http/post (url+ "/kittens_and_puppies") {:throw-exceptions false})]
+         response => (contains {:status 200})))
+
+ (fact "Retrieve message from topic returns 200 HTTP response"
+       (let [response (http/get (url+ "/kittens_and_puppies/alice") {:throw-exceptions false})]
+         response => (contains {:status 200})))
+
+ (fact "Retrieve message from empty topic returns 204 HTTP response"
+       (let [response (http/get (url+ "/kittens_and_puppies/alice") {:throw-exceptions false})]
+         response => (contains {:status 204})))
+
+ (fact "Retrieve message from unsubscribed topic returns 404 HTTP response"
+       (let [response (http/get (url+ "/kittens_and_puppies/carol") {:throw-exceptions false})]
+         response => (contains {:status 404})))
+
+)
